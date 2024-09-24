01e91adf3f2fa8a2c7c76025aae3b221d98358b0
davidchanzz <tovliskatsi@gmail.com>
2024-09-24 11:15:36 +0400

    Init

---
+++ src/main/java/com/gn/testtaskriga/TestTaskRigaApplication.java
@@ -0,0 +1,13 @@
+package com.gn.testtaskriga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskRigaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskRigaApplication.class, args);
+    }
+
+}
